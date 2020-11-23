aaca5c7aaea64bbe28314b09add1c7e69b7e75ab
GraphGrailAi <wisejoomler@gmail.com>
2020-11-23 19:50:38 +0300

    Жириновский: датасет расширен - radiokp, песни, лекции и др

--- convert.py
+++ convert.py
@@ -24,7 +24,7 @@ print(deEmojify(text))
 
 
 txt_file = 'data\girik_txt\Jirinovskiyi_V._Antikissindjer.txt'
-def convert_b17(txt_file, n, m):
+def convert_1stver(txt_file, n, m):
     """ Берем тестовый файл и составляем на его основе формат all_essays.jsonl
     {"text": "Тема: Комедия или драма? (по пьесе А.С. Грибоедова «Горе от ума»)\nСочинение: Грибоедов рассматривал театр
     1) Просто весь текст файла помещаем в поле "text"
@@ -51,5 +51,46 @@ def convert_b17(txt_file, n, m):
             except:
                 print('encode error')
 
-convert_b17(txt_file, 30000, 33000)
+#convert_1stver(txt_file, 30000, 33000)
 
+def convert_2ndver(path):
+    """ Берем тестовый файл и составляем на его основе формат all_essays.jsonl
+    {"text": "Тема: Комедия или драма? (по пьесе А.С. Грибоедова «Горе от ума»)\nСочинение: Грибоедов рассматривал театр
+    2) Вариант 2, сделать сплит по "Владимир Вольфович Жириновский" в каждом файле и брать "Тема: краткое содержание книги или начало вразы, а \nСочинение: полный текст книги.
+    а лучше по абзацам
+    """
+    results = []
+    all_files = os.listdir(path)
+    print(all_files)
+    for file_name in all_files:
+        file_name_path = path + "/" + file_name
+        with open(file_name_path, mode="r", encoding="windows-1251", errors = 'backslashreplace') as csvfile:  # windows-1251
+            #print(csvfile.read())
+            raw_text = csvfile.read()
+            # try:
+            #     split_text = 'Владимир Вольфович Жириновский'
+            #     question = raw_text.split(split_text)[0]
+            #     answer = raw_text.split(split_text)[1]
+            # except:
+
+            print('no Владимир Вольфович Жириновский')
+            split_text = '\n\n'
+            question = ' '.join(raw_text.splitlines()[0:7])
+            print('question', question)
+            answer = ' '.join(raw_text.splitlines()[6:5000])
+            print('answer', answer)
+
+            #print(raw_text)
+            jsonl_string = {"text":"Скажи:{}\nВ. Жириновский:{}".format(question.strip().lstrip().rstrip().replace('\n', ' ').replace('\r', '').replace("'","").replace("--",""),answer.strip().lstrip().rstrip().replace('\n', ' ').replace('\r', '').replace("'","").replace("--",""))}
+            print(jsonl_string)
+            results.append(jsonl_string)
+
+    with open('data/girik_all2.txt', 'a', encoding="utf-8") as out:
+        for item in results:
+            text = item
+            try:
+                out.write("{}".format(text) + '\n')
+            except:
+                print('encode error')
+
+convert_2ndver("data\girik_txt")
--- scripts/generate_ruGPT3Large.sh
+++ scripts/generate_ruGPT3Large.sh
@@ -13,3 +13,12 @@ python generate_transformers.py \
 
 # Модель эссе
 # python generate_transformers.py --model_type=gpt2 --model_name_or_path=checkpoints_1 --k=500 --p=0.9 --length=100 --num_return_sequences=2
+
+
+# Модель Жириновский
+# python generate_transformers.py --model_type=gpt2 --model_name_or_path=checkpoint-1000 --k=100 --p=0.9 --length=200 --num_return_sequences=3
+
+# python generate_transformers.py --model_type=gpt2 --model_name_or_path=checkpoint-1500 --k=100 --p=0.9 --length=200 --num_return_sequences=3
+
+# python generate_transformers.py --model_type=gpt2 --model_name_or_path=checkpoint-3000 --k=100 --p=0.9 --length=200 --num_return_sequences=3
+
